f72fb9dc9639519ee9a8baa8efeffa3424937b1c
PratyushPoddar07 <shuvampoddar018@gmail.com>
2024-10-22 19:09:12 +0530

    email

---
+++ Project/clrTheClutter.py
@@ -0,0 +1,2 @@
+"""Write a program to clear the cutter insde a folder on you computer.
+You should use os module to rename all the png images from 1.png all the way till n.png where n is the number of png files in that folder. Do the same for other file formats."""
