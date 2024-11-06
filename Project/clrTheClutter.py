a6378b11ef75f73c551e7e6a1af579d8247563b8
PratyushPoddar07 <shuvampoddar018@gmail.com>
2024-11-06 08:39:10 +0530

    clrCutter

--- Project/clrTheClutter.py
+++ Project/clrTheClutter.py
@@ -1,2 +1,12 @@
 """Write a program to clear the cutter insde a folder on you computer.
 You should use os module to rename all the png images from 1.png all the way till n.png where n is the number of png files in that folder. Do the same for other file formats."""
+
+import os
+
+files = os.listdir("sample")
+i=1
+for file in files:
+    if file.endswith(".png"):
+        os.rename(f"sample/{file}",f"sample/{i}.png")
+        print(file)
+        i=i+1
